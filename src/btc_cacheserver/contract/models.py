a1ea055f44a8fc2adfc5cb0162ca483474a4de53
zhangmin <zhangmin@rongshutong.com>
2018-01-09 10:43:19 +0800

    MODIFY: model changed, set some default values

--- src/btc_cacheserver/contract/models.py
+++ src/btc_cacheserver/contract/models.py
@@ -37,19 +37,13 @@ class PlatFormInfo(models.Model):
 class LoanInformation(models.Model):
     platform = models.ForeignKey(PlatFormInfo, on_delete=models.CASCADE)
     order_number = models.CharField(max_length=255, help_text='订单号')
-    apply_amount = models.IntegerField(default=0, help_text='申请金额', blank=True,
-                                       null=True)
-    exact_amount = models.IntegerField(default=0, help_text='实际打款金额',
-                                       blank=True, null=True)
-    reason = models.CharField(max_length=255, blank=True, null=True,
-                              help_text='用途')
-    apply_time = models.DateTimeField(help_text='申请时间',
-                                      blank=True, null=True)
+    apply_amount = models.IntegerField(default=0, help_text='申请金额')
+    exact_amount = models.IntegerField(default=0, help_text='实际打款金额')
+    reason = models.CharField(max_length=255, default="", help_text='用途')
+    apply_time = models.DateTimeField(help_text='申请时间', blank=True, null=True)
     interest = models.IntegerField(default=0, help_text='利率')
-    bank_card = models.CharField(max_length=64, help_text='此次交易所属的银行卡',
-                                 null=True, blank=True)
-    overdue_days = models.IntegerField(default=0, help_text='当前逾期天数',
-                                       blank=True)
+    bank_card = models.CharField(max_length=64, help_text='此次交易所属的银行卡', null=True, blank=True)
+    overdue_days = models.IntegerField(default=0, help_text='当前逾期天数')
     tag = models.BinaryField(max_length=66,blank=True, null=True, help_text="区块链中的唯一标识")
     installment_counter = models.IntegerField(default=0, help_text="分期数")
     repayment_counter = models.IntegerField(default=0, help_text="还款次数")
@@ -75,11 +69,8 @@ class RepaymentInfo(models.Model):
     loan_info = models.ForeignKey(LoanInformation, on_delete=models.CASCADE)
     installment_number = models.IntegerField(blank=True, null=True)
     real_repay_time = models.DateTimeField(blank=True, null=True, help_text="还款时间")
-
-    overdue_days = models.IntegerField(default=0, help_text='当前逾期天数',
-                                       blank=True)
-    real_repay_amount = models.IntegerField(default=0, help_text="实际还款金额",
-                                            blank=True, null=True)
+    overdue_days = models.IntegerField(default=0, help_text='当前逾期天数')
+    real_repay_amount = models.IntegerField(default=0, help_text="实际还款金额")
     repay_amount_type = models.IntegerField(choices=repay_amount_type_t, help_text='还款类型', blank=True)
     tag = models.BinaryField(max_length=66,blank=True, null=True, help_text="区块链中的唯一标识")
 
@@ -90,8 +81,8 @@ class RepaymentInfo(models.Model):
 class TransactionInfo(models.Model):
     cumulativeGasUsed = models.IntegerField(default=0, help_text="累计gas消耗")
     gasUsed = models.IntegerField(default=0, help_text="gas消耗")
-    blockNumber = models.IntegerField(default=0, help_text="区块号")
-    transactionIndex = models.IntegerField(default=0, help_text="区块号")
+    blockNumber = models.IntegerField(default=-1, help_text="区块号")
+    transactionIndex = models.IntegerField(default=-1, help_text="区块号")
     call_from = models.CharField(max_length=64, help_text='合约调用account')
     call_to = models.CharField(max_length=64, help_text='合约地址')
     transactionHash = models.CharField(max_length=70, help_text='交易的hash')
