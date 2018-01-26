3ccb17ba801cd1c3b7d7723049f948318b1889d2
zhangmin <zhangmin@rongshutong.com>
2018-01-26 18:23:24 +0800

    MODIFY: little change for start_consumer script

--- src/start_consumer.sh
+++ src/start_consumer.sh
@@ -2,5 +2,5 @@
 for i in `seq 0 15`
 do
     index=`printf "%x" $i`
-    nohup python3.6 consumer.py $index >> consumer-$index.log 2>&1 &
+    nohup python3.6 consumer.py $index > /dev/null 2>&1 &
 done
